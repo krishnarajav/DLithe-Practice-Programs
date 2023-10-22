62e1ba9ee9d6d462d51adbce646195c3e466b9a7
krishnarajav <118679169+krishnarajav@users.noreply.github.com>
2023-10-22 14:46:40 +0530

    Update 25112022-4.c

--- 25112022-4.c
+++ 25112022-4.c
@@ -1,24 +1,24 @@
-//Program to check whether the given date is valid or not
+//Program to check whether the given day is valid or not
 
 #include<stdio.h>
 int main()
 {
-    int date, month, year; 
+    int day, month, year; 
     printf("Enter date as DD MM YYYY:");
-    scanf("%d%d%d", &date, &month, &year);
-    if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (date >= 1 && date <=31)) //Check for Jan/Mar/May/Jul/Aug/Oct/Dec
+    scanf("%d%d%d", &day, &month, &year);
+    if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day >= 1 && day <=31)) //Check for Jan/Mar/May/Jul/Aug/Oct/Dec
     {
         printf("Given date is valid\n");
     }
-    else if((month == 4 || month == 6 || month == 9 || month == 11) && (date >= 1 && date <=30)) //Check for Apr/Jun/Sep/Nov
+    else if((month == 4 || month == 6 || month == 9 || month == 11) && (day >= 1 && day <=30)) //Check for Apr/Jun/Sep/Nov
     {
         printf("Given date is valid\n");
     }
-    else if((month == 2) && (year % 4 != 0) && (date >= 01 && date <=28)) //Check for month Feb
+    else if((month == 2) && (year % 4 != 0) && (day >= 01 && day <=28)) //Check for month Feb
     {
         printf("Given date is valid\n");
     }
-    else if((month == 2) && (year % 4 == 0) && (date >= 01 && date <=29)) //Check for month Feb and leap year
+    else if((month == 2) && (year % 4 == 0) && (day >= 01 && day <=29)) //Check for month Feb and leap year
     {
         printf("Given date is valid\n");
     }
